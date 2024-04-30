2f3974121e3dffd551445d56b8fb5a63d430c0c8
Benji0106 <112694300+Benji0106@users.noreply.github.com>
2024-04-30 11:15:18 +0200

    Remove Splitpackage Ex from master

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/SplitPackageEg.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmmi.cbse.enemysystem;
-
-public class SplitPackageEg {
-    public static void main(String[] args) {
-        System.out.println("Hello World from SplitPackageEg in Enemy module");
-    }
-}
--- Player/src/main/java/dk/sdu/mmmi/cbse/SplitPackageEg.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmmi.cbse;
-
-public class SplitPackageEg {
-    public static void main(String[] args) {
-        System.out.println("Hello World from SplitPackageEg in Player module!");
-    }
-}
